ddab2b1e417d1d3a59e57ab48cbe4ce0663f396e
Parabeac-Cloud <admin@parabeac.com>
2023-05-31 09:53:09 +0000

    Auto-generated commit by Parabeac Cloud

---
+++ lib/theme/shapater_text_styles.g.dart
@@ -0,0 +1,20 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';
+class ShapaterTextStyles {
+
+static const TextStyle mobileTextMediumRegular = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0,
+  fontFamily: 'Epilogue',
+  decoration: TextDecoration.none,
+  fontStyle: FontStyle.normal,
+)
+;
+}
